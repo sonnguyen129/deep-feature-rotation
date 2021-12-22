34678633c4c76c276a47776a0957efe92649c392
sonnguyen129 <sonhai12092000@gmail.com>
2021-12-22 18:22:50 +0700

    Clean code

--- src/function.py
+++ src/function.py
@@ -9,27 +9,37 @@ import argparse
 from loss import style_content_loss
 
 def clip_0_1(image):
-    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
 
 @tf.function()
 def train_step(image, style_targets, extractor, opt, total_variation_weight, content_targets, 
-                content_weight, style_weight,num_content_layers, num_style_layers):
+                content_weight, style_weight, num_content_layers, num_style_layers):
+    """
+    Params:
+    image(list): List of output images(initialized)
+    style_targets(list): List of style targets
+    extractor: model is used to extract high-level representations derived from the content and style image.
+    opt: optimizer(Adam)
+
+    Returns:
+    Generated images after optimizing process
+    """
     with tf.GradientTape() as tape_1, tf.GradientTape() as tape_2, \
         tf.GradientTape() as tape_3, tf.GradientTape() as tape_4:
-    
+
         outputs_1 = extractor(image[0])
         outputs_2 = extractor(image[1])
         outputs_3 = extractor(image[2])
         outputs_4 = extractor(image[3])
 
         loss_1 = style_content_loss(outputs_1, style_targets[0], content_targets, 
-                content_weight, style_weight,num_content_layers, num_style_layers)
+                        content_weight, style_weight,num_content_layers, num_style_layers)
         loss_2 = style_content_loss(outputs_2, style_targets[1], content_targets, 
-                content_weight, style_weight,num_content_layers, num_style_layers)
+                        ontent_weight, style_weight,num_content_layers, num_style_layers)
         loss_3 = style_content_loss(outputs_3, style_targets[2], content_targets, 
-                content_weight, style_weight,num_content_layers, num_style_layers)
+                        content_weight, style_weight,num_content_layers, num_style_layers)
         loss_4 = style_content_loss(outputs_4, style_targets[3], content_targets, 
-                content_weight, style_weight,num_content_layers, num_style_layers)
+                        content_weight, style_weight,num_content_layers, num_style_layers)
 
         loss_1 += total_variation_weight*tf.image.total_variation(image[0])
         loss_2 += total_variation_weight*tf.image.total_variation(image[1])
--- src/loss.py
+++ src/loss.py
@@ -6,19 +6,29 @@ import functools
 import os
 import copy
 
-def style_content_loss(outputs, style_targets, content_targets, content_weight, style_weight,
-            num_content_layers, num_style_layers):
+def calc_style_loss(outputs, style_targets, style_weight,
+                    num_style_layers):
     style_outputs = outputs['style']
-    content_outputs = outputs['content']
     style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
                            for name in style_outputs.keys()])
     style_loss *= style_weight / num_style_layers
+    return style_loss
 
+def calc_content_loss(outputs, content_targets, content_weight, 
+                     num_content_layers):
+    content_outputs = outputs['content']
     content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
                              for name in content_outputs.keys()])
     content_loss *= content_weight / num_content_layers
-    loss = style_loss + content_loss
-    return loss
+    return content_loss
+
+def style_content_loss(outputs, style_targets, content_targets, 
+                content_weight, style_weight, num_content_layers, num_style_layers):
+    style_loss = calc_style_loss(outputs, style_targets, style_weight,
+                    num_style_layers)
+    content_loss = calc_content_loss(outputs, content_targets, content_weight, 
+                     num_content_layers)
+    return style_loss + content_loss
 
 def total_variation_loss(image):
     x_deltas, y_deltas = high_pass_x_y(image)
