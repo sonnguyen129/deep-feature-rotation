888735f66cdfabf3a075fa01e672eb2ebeca0314
sonnguyen129 <sonhai12092000@gmail.com>
2021-12-30 23:08:44 +0700

    Add extensive rotation module

--- src/rotation.py
+++ src/rotation.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from scipy.ndimage import rotate
 
 def rotation_tensor(inputs, angles = None):
     """Rotate tensor in 90, 180, 270 angles"""
@@ -34,3 +35,16 @@ def rotation_tensor(inputs, angles = None):
 
     if angles not in [90, 180, 270]:
         raise ValueError('Invalid value')
+
+def extensive_rotation_tensor(inputs, angles = None):
+    """Rotate tensor in arbitrary angles"""
+    final = copy.deepcopy(inputs)
+
+    for key, input in enumerate(inputs):
+        input = input.numpy()
+        # input_shape = input.shape
+        # x90 = input.transpose((3, 2, 1 ,0)).reshape(input_shape)
+        r_tensor = rotate(input, angles, axes = (1,2), reshape= False)
+        r_tensor = tf.convert_to_tensor(r_tensor)
+        final[key] = r_tensor
+    return final
